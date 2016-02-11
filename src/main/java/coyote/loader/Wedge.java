34961bb96f1af8f6315354b151e613c6f8e8612c
Steve Cote <sdcote@gmail.com>
2016-02-11 07:43:13 -0500

    Show complete lifecycle

--- src/main/java/coyote/loader/Wedge.java
+++ src/main/java/coyote/loader/Wedge.java
@@ -36,30 +36,57 @@ public class Wedge extends AbstractManagedComponent implements ManagedComponent 
   public void setConfiguration( Config config ) {
     super.setConfiguration( config );
     Log.info( config.toFormattedString() );
-    
-    // pause 5 seconds between calls to doWork()
-    setIdleWait( 5000 );
-    
-    // Start out idling
-    setIdle( true );
-    
-    Log.error("Ha!, just kidding");
   }
 
 
 
 
   /**
+   * Called just before we enter the main run loop.
+   * 
+   * @see coyote.loader.thread.ThreadJob#initialize()
+   */
+  @Override
+  public void initialize() {
+    // pause 5 seconds between calls to doWork()
+    setIdleWait( 5000 );
+
+    // Start out idling
+    setIdle( true );
+  }
+
+
+
+
+  /**
+   * This is the main, re-entrant method that is repeatedly called while in the 
+   * main run loop.
    * @see coyote.loader.thread.ThreadJob#doWork()
    */
   @Override
   public void doWork() {
     Log.info( "Doing work..." );
+    Log.trace( "Trace" );
     Log.debug( "Debug" );
     Log.warn( "Warn" );
     Log.error( "Error" );
     Log.fatal( "Fatal" );
+    // some other event categories
     Log.append( "SCHEDULER", "Schedule" );
+    Log.append( "THREAD", "Thread" );
+  }
+
+
+
+
+  /**
+   * Called after the main run loop is exited, performs any resource clean up.
+   * 
+   * @see coyote.loader.thread.ThreadJob#terminate()
+   */
+  @Override
+  public void terminate() {
+    // nothing to do
   }
 
 }
