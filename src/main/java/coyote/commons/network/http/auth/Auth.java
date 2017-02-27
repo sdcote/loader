5c69f5e0442bf8c5bf17c7809dc369dd8f8c11c6
Steve Cote <sdcote@gmail.com>
2017-02-27 17:36:00 -0500

    Adding annotation for security

---
+++ src/main/java/coyote/commons/network/http/auth/Auth.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and implementation
+ */
+package coyote.commons.network.http.auth;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+
+/**
+ * 
+ */
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Auth {
+  
+  public String groups() default "";
+
+
+
+
+  public boolean requireSSL() default false;
+  
+  
+}
