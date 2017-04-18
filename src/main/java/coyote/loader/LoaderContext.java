9f78fa5322a08a94d0de944456ee49204d2048a1
Steve Cote <sdcote@gmail.com>
2017-04-18 14:30:56 -0400

    formatting only

--- src/main/java/coyote/loader/LoaderContext.java
+++ src/main/java/coyote/loader/LoaderContext.java
@@ -14,6 +14,6 @@ package coyote.loader;
 /**
  * 
  */
-public class LoaderContext extends AbstractContext implements Context{
+public class LoaderContext extends AbstractContext implements Context {
 
 }
