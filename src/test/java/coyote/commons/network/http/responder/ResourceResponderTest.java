d5ac572adf8b9a1cabbbba633ef5a3560568084d
sdcote <sdcote@aep.com>
2019-03-20 16:29:06 -0400

    Updated unit tests to use standard configuration approach (server,Config)

--- src/test/java/coyote/commons/network/http/responder/ResourceResponderTest.java
+++ src/test/java/coyote/commons/network/http/responder/ResourceResponderTest.java
@@ -1,35 +1,36 @@
 /*
  * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the MIT License which accompanies this distribution, and is 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which accompanies this distribution, and is
  * available at http://creativecommons.org/licenses/MIT/
  *
  * Contributors:
- *   Stephan D. Cote 
+ *   Stephan D. Cote
  *      - Initial concept and implementation
  */
 package coyote.commons.network.http.responder;
 
 //import static org.junit.Assert.*;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.io.IOException;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import coyote.commons.NetUtil;
 import coyote.commons.network.http.HTTPD;
 import coyote.commons.network.http.TestHttpClient;
 import coyote.commons.network.http.TestResponse;
 import coyote.commons.network.http.TestRouter;
+import coyote.loader.cfg.Config;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 
 /**
- * 
+ *
  */
 public class ResourceResponderTest {
 
@@ -37,35 +38,31 @@ public class ResourceResponderTest {
   private static int port = 3232;
 
 
-
-
   /**
    * @throws java.lang.Exception
    */
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    port = NetUtil.getNextAvailablePort( port );
-    server = new TestRouter( port );
+    port = NetUtil.getNextAvailablePort(port);
+    server = new TestRouter(port);
     server.addDefaultRoutes();
 
     // try to start the server, waiting only 2 seconds before giving up
     try {
-      server.start( HTTPD.SOCKET_READ_TIMEOUT, true );
+      server.start(HTTPD.SOCKET_READ_TIMEOUT, true);
       long start = System.currentTimeMillis();
-      Thread.sleep( 100L );
-      while ( !server.wasStarted() ) {
-        Thread.sleep( 100L );
-        if ( System.currentTimeMillis() - start > 2000 ) {
+      Thread.sleep(100L);
+      while (!server.wasStarted()) {
+        Thread.sleep(100L);
+        if (System.currentTimeMillis() - start > 2000) {
           server.stop();
-          fail( "could not start server" );
+          fail("could not start server");
         }
       }
-    } catch ( IOException ioe ) {
-      fail( "could not start server" );
+    } catch (IOException ioe) {
+      fail("could not start server");
     }
   }
-
-
 
 
   /**
@@ -77,31 +74,29 @@ public class ResourceResponderTest {
   }
 
 
-
-
   @Test
   public void test() {
-    server.addRoute( "/", Integer.MAX_VALUE, ResourceResponder.class, "content" );
-    server.addRoute( "/(.)+", Integer.MAX_VALUE, ResourceResponder.class, "content", true );
+    server.addRoute("/", Integer.MAX_VALUE, ResourceResponder.class, server, new Config().set(ResourceResponder.ROOT_TAG, "content"));
+    server.addRoute("/(.)+", Integer.MAX_VALUE, ResourceResponder.class, server, new Config().set(ResourceResponder.ROOT_TAG, "content").set(ResourceResponder.REDIRECT_TAG, true));
 
     try {
-      TestResponse response = TestHttpClient.sendGet( "http://localhost:" + port );
-      System.out.println( response.getStatus() + ":" + response.getData() );
-      assertTrue( response.getStatus() == 200 );
-      assertTrue( server.isAlive() );
+      TestResponse response = TestHttpClient.sendGet("http://localhost:" + port);
+      System.out.println(response.getStatus() + ":" + response.getData());
+      assertTrue(response.getStatus() == 200);
+      assertTrue(server.isAlive());
 
-      response = TestHttpClient.sendGet( "http://localhost:" + port + "/" );
-      System.out.println( response.getStatus() + ":" + response.getData() );
-      assertTrue( response.getStatus() == 200 );
-      assertTrue( server.isAlive() );
+      response = TestHttpClient.sendGet("http://localhost:" + port + "/");
+      System.out.println(response.getStatus() + ":" + response.getData());
+      assertTrue(response.getStatus() == 200);
+      assertTrue(server.isAlive());
 
-      response = TestHttpClient.sendGet( "http://localhost:" + port + "/notfound.txt" );
-      System.out.println( response.getStatus() + ":" + response.getData() );
-      assertTrue( response.getStatus() == 404 );
-      assertTrue( server.isAlive() );
+      response = TestHttpClient.sendGet("http://localhost:" + port + "/notfound.txt");
+      System.out.println(response.getStatus() + ":" + response.getData());
+      assertTrue(response.getStatus() == 404);
+      assertTrue(server.isAlive());
 
-    } catch ( Exception e ) {
-      fail( e.getMessage() );
+    } catch (Exception e) {
+      fail(e.getMessage());
     }
 
   }
