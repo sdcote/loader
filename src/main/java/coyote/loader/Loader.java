d9a51c393045c8a89c5ad5b483f5a364ee37201a
Steve Cote <sdcote@gmail.com>
2019-03-25 21:22:56 -0400

    Incremented to version 0.7.19

--- src/main/java/coyote/loader/Loader.java
+++ src/main/java/coyote/loader/Loader.java
@@ -24,7 +24,7 @@ import coyote.loader.thread.ThreadPool;
  */
 public interface Loader extends WatchDog {
 
-  Version API_VERSION = new Version(0, 7, 18, Version.GENERAL);
+  Version API_VERSION = new Version(0, 7, 19, Version.GENERAL);
   String API_NAME = "CoyoteLoader";
   String LOADER = "Loader";
   String INITIALIZING = "Initializing";
